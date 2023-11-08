5eef202d5ee921be155007818685d366debe8d67
Andra <aandraaa24@gmail.com>
2023-11-08 14:47:33 +0800

    updet

--- PRAK404-2310817210001-AndraBraputraAkbarSaleh.c
+++ PRAK404-2310817210001-AndraBraputraAkbarSaleh.c
@@ -2,7 +2,7 @@
 
 int main() {
     float a, b;
-    char pil;
+    int pil;
 
     while (1) {
         printf("Pilih operasi:\n");
@@ -12,26 +12,24 @@ int main() {
         printf("4. Pembagian\n");
         printf("5. Exit\n");
         printf("Masukkan pilihan : ");
-        scanf(" %c", &pil);
+        scanf("%d", &pil);
 
-        if (pil == '1' || pil == '2' || pil == '3' || pil == '4') {
+        if (pil == 1 || pil == 2 || pil == 3 || pil == 4) {
             printf("Masukkan angka pertama: ");
             scanf("%f", &a);
             printf("Masukkan angka kedua: ");
             scanf("%f", &b);
 
-            if (pil == '1') {
+            if (pil == 1) {
                 printf("Hasil pertambahan antara %.2f dengan %.2f adalah %.2f\n", a, b, a + b);
-            } else if (pil == '2') {
+            } else if (pil == 2) {
                 printf("Hasil pengurangan antara %.2f dengan %.2f adalah %.2f\n", a, b, a - b);
-            } else if (pil == '3') {
+            } else if (pil == 3) {
                 printf("Hasil perkalian antara %.2f dengan %.2f adalah %.2f\n", a, b, a * b);
-            } else if (pil == '4') {
-                if (b != 0) {
+            } else if (pil == 4) {
                     printf("Hasil pembagian antara %.2f dengan %.2f adalah %.2f\n", a, b, a / b);
-                }
             }
-        } else if (pil == '5') {
+        } else if (pil == 5) {
             printf("Terimakasih, telah menggunakan kalkulator Andra.\n");
             break;
         } else {
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+a = input("")
+a = int(a)
+
+for i in range(1, a + 1):
+    if i % 2 != 0:
+        print(i, end=' ')
+print()
+for i in range(a, 0, -1):
+    if i % 2 == 0:
+        print(i, end=' ')
